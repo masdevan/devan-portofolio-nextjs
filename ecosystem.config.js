8dd128a3f5de3de6b2fd79b4f22b66c42add5cde
masdevan <masdevansugiharta@gmail.com>
2025-08-13 21:46:55 +0700

    update ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "production",
+      script: "npm",
+      args: "run start",
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+}
